5780ec7b395807bd9ef1eabd13eedda94c9832df
Songkeys <songv587@gmail.com>
2023-03-15 19:44:13 +0800

    chore: prettier

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-  defaultShowCopyCode: true
+	theme: 'nextra-theme-docs',
+	themeConfig: './theme.config.tsx',
+	defaultShowCopyCode: true,
+	staticImage: false
 })
 
-module.exports = withNextra()
+module.exports = withNextra({
+	images: {
+		unoptimized: true
+	}
+})
